f3f542779f9dc7d24a7624d8bdd6ad4c3c27d3df
DarkKnightof1 <mischa@boskat.eu>
2017-11-07 09:37:57 +0100

    Op122 java

---
+++ src/h12/Op122.java
@@ -0,0 +1,28 @@
+package h12;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Op122 extends Applet {
+
+    Button knop[];
+
+
+    public void init() {
+        knop = new Button [25];
+
+        for (int teller = 0; teller < knop.length; teller ++) {
+           Button b = new Button();
+           knop[teller] = b;
+           add(b);
+        }
+    }
+
+    //public void paint(Graphics g) {
+        //for (int teller = 0; teller < gemiddelde.length; teller ++) {
+            //g.drawString("" + gemiddelde[teller], 50, 20 * teller + 20);
+            //g.drawString("gemiddelde:" + uitkomst[teller], 90, 20* teller + 20);
+        //}
+    //}
+}
