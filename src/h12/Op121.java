55c301cb1923d22e3536819e04fb7aa1b7131401
DarkKnightof1 <mischa@boskat.eu>
2017-11-09 12:55:43 +0100

    Op 1 H12

--- src/h12/Op121.java
+++ src/h12/Op121.java
@@ -6,21 +6,26 @@ import java.applet.*;
     public class Op121 extends Applet {
         double gemiddelde[];
         double uitkomst[];
+        double getal;
+        double avg;
 
         public void init() {
             gemiddelde = new double [10];
             uitkomst = new double[10];
-
+            getal = 0.00;
             for (int teller = 0; teller < gemiddelde.length; teller ++) {
                 gemiddelde[teller] = teller + 1;
-               uitkomst[teller] = (teller + 1);
+                getal += gemiddelde[teller];
             }
+            avg = (getal/gemiddelde.length);
         }
 
         public void paint(Graphics g) {
             for (int teller = 0; teller < gemiddelde.length; teller ++) {
                 g.drawString("" + gemiddelde[teller], 50, 20 * teller + 20);
-                g.drawString("gemiddelde:" + uitkomst[teller], 90, 20* teller + 20);
             }
+                g.drawString("gemiddelde = " + avg, 120, 20);
+
+            //g.drawString("gemiddelde:" + (gemiddelde[teller]/10), 90, 20);
         }
     }
